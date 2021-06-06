37c335da4ef99056f592c2c95db1e491a96fe69c
danaee <danaee4421@gmail.com>
2021-06-06 12:22:20 +0430

    fix bug run

--- src/main/java/config/ImageConfig.java
+++ src/main/java/config/ImageConfig.java
@@ -1,33 +1,10 @@
-package config;
+package Config;
 
 public class ImageConfig {
-    public static String backgroundImage = "Image/Background/background.png";
+    public static String bullet = "Image/bullet/bullet.png";
 
-    public static String peashooter = "Image/peashooter/Frame0.png";
-
-    public static String icePeashooter = "Image/peashooter/Frame0.png";
-
-    public static String sunflower = "Image/sunflower/Frame0.png";
-
-    public static String nut1 = "Image/nut1/Frame0.png";
-
-    public static String shoppingImage = "Image/Background/shopping.png";
-
-    public static String dieImage = "/die.jpg";
-
-    public static String winImage = "/win.jpg";
-
-    public static String registerImage = "/Plants-vs-Zombies-3.jpg";
-
-    public static String prepareImage = "/Images/start3.png";
-
-    public static String loginImage = "/Plants-vs-Zombies-3.jpg";
-
-    public static String mainMenuImage = "/Start.jpeg";
-
-    public static String bulletImage = "Image/bullet/bullet.png";
-
-    public static String cardImage = "Image/card/";
+    public static String card = "Image/card/";
+<<<<<<< HEAD
 
     public static String darkZombieFrame = "Image/darkzombie/Frame";
 
@@ -35,47 +12,113 @@ public class ImageConfig {
 
     public static String darkZombieDieFrame = "Image/darkzombiedie/Frame";
 
-    public static String darkZombieHeadFrame = "Image/darkzombiehead/Frame";
-
-    public static String iceBulletImage = "Image/bullet/icebullet.png";
+    public static String iceBullet = "Image/bullet/icebullet.png";
 
     public static String zombieFrame = "Image/zombie/Frame";
 
     public static String zombieEatFrame = "Image/zombieeat/Frame";
 
-    public static String zombieFrozenImage = "Image/zombieFrozen.png";
+    public static String zombieFrozen = "Image/zombieFrozen.png";
 
-    public static String peashooterImage = "Image/peashooter/Frame";
+    public static String zombieDieFrame = "Image/zombiedie/Frame";
+<<<<<<< HEAD
 
-    public static String plantPutPeashooterImage = "Image/plantput/pea.png";
+    public static String zombieHeadFrame = "Image/zombiehead/Frame";
 
-    public static String icePeashooterImage = "Image/icepeashooter/Frame";
+    public static String peashooterFrame = "Image/peashooter/Frame";
 
-    public static String icePlantPutImage = "Image/plantput/ice.png";
+    public static String peashooterFrame2 = "Image/peashooter/Frame0.png";
 
+    public static String icePeashooterFrame = "Image/icepeashooter/Frame";
+=======
+>>>>>>> origin/main
+
+=======
+
+    public static String zombieHeadFrame = "Image/zombiehead/Frame";
+
+<<<<<<< HEAD
+    public static String peashooterFrame = "Image/peashooter/Frame";
+
+    public static String peashooterFrame2 = "Image/peashooter/Frame0.png";
+=======
+    public static String darkZombieDieFrame = "Image/darkzombiedie/Frame";
+
+    public static String iceBullet = "Image/bullet/icebullet.png";
+>>>>>>> origin/main
+
+    public static String icePeashooterFrame = "Image/icepeashooter/Frame";
+
+>>>>>>> 9f91c1e (fix bug run)
+    public static String icePeashooterFrame2 = "Image/icepeashooter/Frame0.png";
+
+<<<<<<< HEAD
     public static String sunflowerFrame = "Image/sunflower/Frame";
+
+    public static String sunflowerFrame2  = "Image/sunflower/Frame0.png";
 
     public static String plantPutSun = "Image/plantput/sun.png";
 
-    public static String nut1Frame = "Image/nut1/Frame";
+    public static String nutFrame = "Image/nut1/Frame";
 
     public static String nut2Frame = "Image/nut2/Frame";
+=======
+    public static String zombieFrozen = "Image/zombieFrozen.png";
+
+    public static String zombieDieFrame = "Image/zombiedie/Frame";
+
+    public static String zombieHeadFrame = "Image/zombiehead/Frame";
+
+    public static String peashooterFrame = "Image/peashooter/Frame";
+
+    public static String peashooterFrame2 = "Image/peashooter/Frame0.png";
+
+    public static String icePeashooterFrame = "Image/icepeashooter/Frame";
+
+    public static String icePeashooterFrame2 = "Image/icepeashooter/Frame0.png";
+>>>>>>> origin/main
 
     public static String nut3Frame = "Image/nut3/Frame";
 
-    public static String plantPutSunImage = "Image/plantput/nut.png";
+<<<<<<< HEAD
+    public static String nut1Frame2 = "Image/nut1/Frame0.png";
+<<<<<<< HEAD
 
-    public static String redZombieFrame = "Image/redzombie/Frame";
+    public static String plantPutNut = "Image/plantput/nut.png";
+=======
+    public static String sunflowerFrame2  = "Image/sunflower/Frame0.png";
 
-    public static String redZombieEatFrame = "Image/redzombieeat/Frame";
+    public static String plantPutSun = "Image/plantput/sun.png";
+
+    public static String nutFrame = "Image/nut1/Frame";
+>>>>>>> origin/main
+
+    public static String redZombie = "Image/redzombie/Frame";
+
+=======
+
+<<<<<<< HEAD
+=======
+    public static String nut1Frame2 = "Image/nut1/Frame0.png";
+
+>>>>>>> origin/main
+    public static String plantPutNut = "Image/plantput/nut.png";
+
+    public static String redZombie = "Image/redzombie/Frame";
+
+<<<<<<< HEAD
+>>>>>>> 9f91c1e (fix bug run)
+=======
+>>>>>>> origin/main
+    public static String redZombieEat = "Image/redzombieeat/Frame";
 
     public static String redZombieDie = "Image/redzombiedie/Frame";
 
     public static String redZombieHead = "Image/redzombiehead/Frame";
 
-    public static String sunImage = "Image/sun.png";
+    public static String sun = "Image/sun.png";
 
-    public static String yellowZombieFrame = "Image/yellowzombie/Frame";
+    public static String yellowZombie = "Image/yellowzombie/Frame";
 
     public static String yellowZombieEat = "Image/yellowzombieeat/Frame";
 
@@ -83,5 +126,32 @@ public class ImageConfig {
 
     public static String yellowZombieHead = "Image/yellowzombiehead/Frame";
 
+    public static String dieField = "/die.jpg";
 
+    public static String LoginAndRegister = "/Plants-vs-Zombies-3.jpg";
+
+    public static String preparePanel = "/start3.png";
+
+    public static String winField = "/win.jpg";
+
+    public static String background = "Image/Background/background.png";
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> origin/main
+
+    public static String backgroundShopping = "Image/Background/shopping.png";
+
+    public static String plantPutPea = "Image/plantput/pea.png";
+<<<<<<< HEAD
+
+    public static String plantPutIce = "Image/plantput/ice.png";
+=======
+
+    public static String backgroundShopping = "Image/Background/shopping.png";
+>>>>>>> 9f91c1e (fix bug run)
+=======
+
+    public static String plantPutIce = "Image/plantput/ice.png";
+>>>>>>> origin/main
 }
--- src/main/java/gui/PlantsVsZombies.java
+++ src/main/java/gui/PlantsVsZombies.java
@@ -1,151 +1,177 @@
 package gui;
 
-import gui.DieField;
-import gui.GameField;
-import gui.PreparePanel;
-import gui.WinField;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class PlantsVsZombies {
-    int runstate = 0;
+    int runstate=0;
 
     GameField pvz_game;
     PreparePanel pvz_pre;
-    DieField pvz_die;
-    WinField pvz_win;
+
+    DieField   pvz_die;
+    WinField   pvz_win;
 
 
+    void newGame() {
+        pvz_pre=new PreparePanel();
+    }
     public void mouseclick(MouseEvent e) {
-        if (pvz_game != null) pvz_game.mouseclick(mx, my);
+        if(pvz_game!=null) pvz_game.mouseclick(mx, my);
     }
 
-    void mouserelease(MouseEvent e) {
-    }
-
-    void mousemove(MouseEvent e) {
-        if (pvz_game != null) {
+    void mouserelease(MouseEvent e) {}
+    void mousemove(MouseEvent e)
+    {
+        if(pvz_game!=null)
+        {
             pvz_game.mouse_move(mx, my);
         }
     }
+    void mousedrag(MouseEvent e) {}
+    void panel(Graphics g)
+    {
 
-    void mousedrag(MouseEvent e) {
-    }
-
-    void panel(Graphics g) {
-        if (pvz_game != null) {
+        if(pvz_game!=null)
+        {
             pvz_game.gamepanel(g);
         }
+//        if(pvz_pre!=null)
+//        {
+//            pvz_pre.PreparePanel(g);
+//        }
+//        if(pvz_die!=null) {
+//            pvz_die.diefield(g);
+//        }
+//        if(pvz_win!=null) {
+//            pvz_win.winfield(g);
+//        }
     }
-
     void timer() throws Exception {
-        if (pvz_game != null) {
+        if(pvz_game!=null)
+        {
             pvz_game.timer();
         }
     }
 
 
-    mainPanel panel = null;
-    mainTimer timer = null;
+    mainPanel panel=null;
+    mainTimer timer=null;
 
-    int mx, my;
 
-    public PlantsVsZombies() {
-        panel = new mainPanel();
-        panel.setSize(860, 678 + 35);
+    int mx,my;//Êó±êºá×Ý×ø±ê
+    public PlantsVsZombies(){
+        newGame();
+        panel=new mainPanel();
+        panel.setSize(860, 678+35);
         panel.setVisible(true);
         panel.setLocationRelativeTo(null);
-        timer = new mainTimer(59);
+        timer=new mainTimer(59);
+
     }
 
-    class mainPanel extends javax.swing.JFrame {
-        mainPanelListener a = null;
-        window b = null;
-
-        mainPanel() {
-            b = new window();
+    class mainPanel extends JFrame {
+        mainPanelListener  a= null;
+        window b=null;
+        mainPanel(){
+            b=new window();
             this.add(b);
-            a = new mainPanelListener();
+            a=new mainPanelListener();
             this.addWindowListener(a);
             this.repaint();
         }
     }
-
-    class mainPanelListener extends WindowAdapter {
-        public void windowClosing(WindowEvent e) {
+    class mainPanelListener extends WindowAdapter
+    {
+        public void windowClosing(WindowEvent e)
+        {
             System.exit(0);
         }
     }
 
 
-    class window extends JPanel {
-        mouseListener a = null;
+    class window extends JPanel
+    {
+        mouseListener						a					= null;
 
-        window() {
+        window()
+        {
             a = new mouseListener();
             this.addMouseListener(a);
             this.addMouseMotionListener(a);
         }
 
-        class mouseListener extends MouseAdapter implements MouseMotionListener {
-            public void mousePressed(MouseEvent e) {
+        class mouseListener extends MouseAdapter implements MouseMotionListener
+        {
+            public void mousePressed(MouseEvent e)
+            {
                 mx = e.getX();
                 my = e.getY();
                 mouseclick(e);
                 panel.repaint();
             }
 
-            public void mouseDragged(MouseEvent e) {
+            public void mouseDragged(MouseEvent e)
+            {
                 mx = e.getX();
                 my = e.getY();
                 mousedrag(e);
                 panel.repaint();
             }
 
-            public void mouseMoved(MouseEvent e) {
+            public void mouseMoved(MouseEvent e)
+            {
                 mx = e.getX();
                 my = e.getY();
                 mousemove(e);
                 panel.repaint();
             }
 
-            public void mouseReleased(MouseEvent e) {
+            public void mouseReleased(MouseEvent e)
+            {
                 mx = e.getX();
                 my = e.getY();
                 mouserelease(e);
                 panel.repaint();
             }
 
-            public void mouseClicked(MouseEvent e) {
+            public void mouseClicked(MouseEvent e)
+            {
             }
         }
 
-        public void paint(Graphics g) {
+        public void paint(Graphics g)
+        {
             panel(g);
 
         }
     }
 
-    class mainTimer implements Runnable {
-        Thread xc = null;
-        long a;
+    class mainTimer implements Runnable
+    {
+        Thread	xc	= null;
+        long	a;
 
-        mainTimer(long j) {
+        mainTimer(long j)
+        {
             this.a = j;
             xc = new Thread(this);
             xc.start();
         }
 
-        public void run() {
-            while (true) {
+        public void run()
+        {
+            while(true) {
                 System.out.print("");
-                while (runstate == 0) {
-                    try {
+                while (runstate==0)
+                {
+                    try
+                    {
                         Thread.sleep(a);
 
-                        if (this == timer) {
+                        if (this == timer)
+                        {
 
                             try {
                                 timer();
@@ -156,11 +182,12 @@ public class PlantsVsZombies {
 
                             panel.repaint();
                         }
-                    } catch (InterruptedException e) {
+                    }
+                    catch (InterruptedException e)
+                    {
                         e.printStackTrace();
                     }
                 }
-            }
-        }
+            }}
     }
 }
